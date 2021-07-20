6311167c62a43804a16dd3e5b559f18b1b9b4d7c
tarrog <tarrog312@gmail.com>
2021-07-20 17:05:44 +0900

    add login:

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const config = require('./config/key')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://tests:abcd1234@cluster0.n6uhb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -17,6 +22,21 @@ mongoose.connect('mongodb+srv://tests:abcd1234@cluster0.n6uhb.mongodb.net/myFirs
 app.get('/', (req, res) => {
     res.send('success');
 });
+
+
+const Users = require('./models/user');
+app.post('/register', (req, res) => {
+    const user = new Users(req.body);
+    
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        });
+    });
+
+});
+
 
 app.listen(port, () => {
     console.log('listening at port' + port);
